681aa1ec45bdc13f4b1dae9b412dda839b537118
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-12-15 11:45:30 +0530

    :lipstick: cart ui completed

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -3,12 +3,20 @@ import { ShoppingCart } from "lucide-react";
 import {
   Sheet,
   SheetContent,
+  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { cn, formalPrice } from "@/lib/utils";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import Image from "next/image";
 
 export const Cart = () => {
+  const itemcount = 0;
+  const fee = 100;
   return (
     <Sheet>
       <SheetTrigger className="group -m-2 flex items-center p-2">
@@ -21,9 +29,69 @@ export const Cart = () => {
           0
         </span>
       </SheetTrigger>
-      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg ">
-        <SheetHeader className="space-y-2.5 pr-6">
+      <SheetContent className="flex w-full flex-col pr-0  sm:max-w-lg ">
+        <SheetHeader className="space-y-2.5">
           <SheetTitle>Cart (0)</SheetTitle>
+          {itemcount > 0 ? (
+            <>
+              <div className="flex w-full  pr-6 ">cart items</div>
+              <div className="space-y-4 pr-6">
+                <Separator />
+                <div className="space-y-6 text-small">
+                  <div className="flex  justify-between ">
+                    <span className=""> shipping</span>
+                    <span>free</span>
+                  </div>
+                  <div className="flex justify-between ">
+                    <span> Transaction Fee</span>
+                    <span>{formalPrice(fee)}</span>
+                  </div>
+                  <div className="flex justify-between ">
+                    <span> Total</span>
+                    <span>{formalPrice(fee)}</span>
+                  </div>
+                </div>
+                <SheetFooter>
+                  <SheetTrigger asChild>
+                    <Link
+                      href="/cart"
+                      className={cn(buttonVariants({ className: "w-full" }))}
+                    >
+                      Continue To CheckOut
+                    </Link>
+                  </SheetTrigger>
+                </SheetFooter>
+              </div>
+            </>
+          ) : (
+            <>
+              <div className="flex h-96 flex-col items-center justify-center space-y-1 ">
+                <div
+                  aria-hidden="true"
+                  className="relative mb-4 h-60 w-60 text-muted-foreground"
+                >
+                  <Image
+                    src="/hippo-empty-cart.png"
+                    fill
+                    alt="empty shopping cart"
+                  />
+                </div>
+                <div className="text-xl font-semibold">Your cart is empty</div>
+                <SheetTrigger asChild>
+                  <Link
+                    href={"/products"}
+                    className={buttonVariants({
+                      variant: "link",
+                      size: "sm",
+                      className: "text-sm text-muted-foreground text-blue-500",
+                    })}
+                  >
+                    Add items to cart to checkout
+                  </Link>
+                </SheetTrigger>
+              </div>
+            </>
+          )}
         </SheetHeader>
       </SheetContent>
     </Sheet>
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,24 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+export function formalPrice(
+  price: number | string,
+  option: {
+    currency?: "USD" | "EUR" | "GBP" | "INR";
+    notation?: Intl.NumberFormatOptions["notation"];
+  } = {}
+) {
+  const { currency = "USD", notation = "compact" } = option;
+
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
+
+  return new Intl.NumberFormat("en-us", {
+    style: "currency",
+    currency,
+    notation,
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
