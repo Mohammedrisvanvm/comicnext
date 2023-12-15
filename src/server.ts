bd8eba2263d70f3b77bcabb8dd90eb0e2d4a8f95
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-12-15 17:39:13 +0530

    :wrench:connected express with nextHandler

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Express from "express";
 import { getPayloadClient } from "./get-payload";
+import { nextApp, nextHandler } from "./next.utils";
 
 const app = Express();
 const port = Number(process.env.PORT) || 3000;
@@ -11,6 +12,17 @@ const start = async () => {
         cms.logger.info(`admin url ${cms.getAdminURL}`);
       },
     },
+  });
+
+  app.use((req, res) => nextHandler(req, res));
+
+  nextApp.prepare().then(() => {
+    payload.logger.info("next.js started");
+    app.listen(port, () => {
+      payload.logger.info(
+        `next.js app url:${process.env.NEXT_PUBLIC_SERVER_URL}`
+      );
+    });
   });
 };
 start();
