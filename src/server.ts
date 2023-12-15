79764c7acc57dae7ceedab449202dd60587dabf5
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-12-15 17:27:15 +0530

    :construction: payload implimented for cache

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,16 @@
 import Express from "express";
+import { getPayloadClient } from "./get-payload";
 
 const app = Express();
 const port = Number(process.env.PORT) || 3000;
+const start = async () => {
+  const payload = await getPayloadClient({
+    initOptions: {
+      express: app,
+      onInit: async (cms) => {
+        cms.logger.info(`admin url ${cms.getAdminURL}`);
+      },
+    },
+  });
+};
+start();
