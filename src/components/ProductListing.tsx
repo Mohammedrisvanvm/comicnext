509f6428f0bc48afa08fd663c4bc5b28b0039caf
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-12-26 10:57:04 +0530

    slider added

--- src/components/ProductListing.tsx
+++ src/components/ProductListing.tsx
@@ -6,6 +6,7 @@ import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { cn, formalPrice } from "@/lib/utils";
 import { PRODUCT_CATEGORIES } from "@/config/intex";
+import ImageSlider from "./ImageSlider";
 
 interface ProductListingProps {
   product: Product | null;
@@ -28,6 +29,7 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
     ({ value }) => value === product.category
   )?.label;
 
+  const validUrls=product.images.map(({image})=>typeof image==='string'?image:image.url).filter(Boolean) as string[]
   if (isVisible && product) {
     return (
       <Link
@@ -37,6 +39,9 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
         href={`/product/${product.id}`}
       >
         <div className="flex flex-col w-full">
+            {/* airbnb  ImageSlider */}
+            {/* <ImageSlider/> */}
+            <ImageSlider urls={validUrls}/>
           <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
           </h3>
