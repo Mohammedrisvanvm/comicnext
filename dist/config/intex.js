597680e854dfd4fcf36e2f2b86e39d07c2acd75b
Mohammedrisvanvm <risvanrishu0000@gmail.com>
2023-12-30 19:04:32 +0530

    build folder added

---
+++ dist/config/intex.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PRODUCT_CATEGORIES = void 0;
+exports.PRODUCT_CATEGORIES = [
+    {
+        label: "UI KITS",
+        value: "ui_kits",
+        featured: [
+            { name: "Editor Picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg" },
+            { name: "New Arrivals", href: "#", imageSrc: "/nav/ui-kits/blue.jpg" },
+            { name: "Best Sellers", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
+        ],
+    },
+    {
+        label: "ICONS",
+        value: "icons",
+        featured: [
+            {
+                name: "Favorite Icon Pick",
+                href: "#",
+                imageSrc: "/nav/icons/picks.jpg",
+            },
+            { name: "New Arrivals", href: "#", imageSrc: "/nav/icons/new.jpg" },
+            {
+                name: "Best Selling Icons",
+                href: "#",
+                imageSrc: "/nav/icons/bestsellers.jpg",
+            },
+        ],
+    },
+];
